b5f5d2cf41bf59040e7e45293e8d06b3fc649521
MarioGarcia7w7 <83962755+MarioGarcia7w7@users.noreply.github.com>
2021-06-15 10:00:48 -0500

    Update DatosModel.php

--- codeingniter/app/Models/DatosModel.php
+++ codeingniter/app/Models/DatosModel.php
@@ -5,6 +5,6 @@ use CodeIgniter\Model;
 class DatosModel extends Model{
     Public function listar(){
       $m = $this->db->query("SELECT * from persona");
-      retunr $m->getResult();
+      return $m->getResult();
     }
 }
