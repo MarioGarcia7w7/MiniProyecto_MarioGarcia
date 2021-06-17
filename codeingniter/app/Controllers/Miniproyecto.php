c6959bb2c884cd08651b1778a465fdbcae5b6d40
MarioGarcia7w7 <83962755+MarioGarcia7w7@users.noreply.github.com>
2021-06-17 13:13:47 -0500

    Update Miniproyecto.php

--- codeingniter/app/Controllers/Miniproyecto.php
+++ codeingniter/app/Controllers/Miniproyecto.php
@@ -2,17 +2,75 @@
 
 namespace App\Controllers;
 use App\Models\DatosModel;
-
-class Miniproyecto extends BaseController
+class General extends BaseController
 {
 	public function index()
 	{
 		$model = new DatosModel();
-		$datos = $model->listar();
+		$mensaje = session('mensaje');
+		$datos = $model->listarTodo();
 		$data = [
-			
-			"datos" => $datos 
+                     "datos" => $datos,
+			"mensaje" => $mensaje
 		];
 		return view('listado',$data);
+	}
+}
+        public function obtenerDatos($id) {
+		$model = new DatosModel();
+		$data = ["id" => $id];
+		$respuesta = $model ->obtenerInformacion($data);
+		
+		$datos = ["datos" => $respuesta];
+		return view('actualizar', $datos);
+		
+	}
+        public function insertar(){
+		$model = new DatosModel();
+		$data = [
+			"nombre" => $_POST ['nombre'],
+			"a_paterno" => $_POST ['apaterno'],
+			"a_materno" => $_POST ['materno'],
+		];
+		$respuesta = $model ->insertar($data);
+		
+		if($respuesta > 0){
+			return redirect()->to(base_url('/index.php'))->with('mensaje','0');
+		}else{
+			return redirect()->to(base_url('/index.php'))->with('mensaje','1');
+		}
+	}
+       
+
+
+
+        public function actualizar(){
+		$model = new DatosModel();
+		$data = [
+			"nombre" => $_POST ['nombre'],
+			"a_paterno" => $_POST ['paterno'],
+			"a_materno" => $_POST ['materno'],
+			
+			
+		];
+		$id = ["id" => $_POST['id']];
+		$respuesta = $model->actualizar($data,$id);
+		
+		if($respuesta){
+			return redirect()->to(base_url('/index.php'))->with('mensaje','2');
+		}else{ 
+			return redirect()->to(base_url('/index.php'))->with('mensaje','3');
+		}
+	}
+        public function eliminar($idPersona){
+		$model = new DatosModel();
+		$id = [ "id" => $idPersona];
+		$respuesta = $model->eliminar($id);
+		
+		if($respuesta){
+			return redirect()->to(base_url('/index.php'))->with('mensaje','4');
+		}else{
+			return redirect()->to(base_url('/index.php'))->with('mensaje','5');
+		}
 	}
 }
