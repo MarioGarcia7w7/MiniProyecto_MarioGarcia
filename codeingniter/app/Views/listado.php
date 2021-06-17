1b4e097023ad2ab86aef2d48c1ab13ae331f2c68
MarioGarcia7w7 <83962755+MarioGarcia7w7@users.noreply.github.com>
2021-06-17 13:23:15 -0500

    Update listado.php

--- codeingniter/app/Views/listado.php
+++ codeingniter/app/Views/listado.php
@@ -58,11 +58,22 @@
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+    <script>
+      let mensaje == '<?php echo $mensaje; ?>';
+      if (mensaje == '0'){
+        swal (':D','registro insertado correctamente', 'success');
+      }else if(mensaje =='1'){
+        swal(':(', 'no fue posible insertar registro correctamente', 'error');
+      }else if(mensaje =='2'){
+        swal(':D', 'registro actualizado correctamente', 'success');
+      }else if(mensaje =='3'){
+        swal(':(', 'no fue posible actualizar el registro correctamente', 'error');
+      }else if(mensaje =='4'){
+        swal(':D', 'registro eliminado correctamente', 'success');
+      }else if(mensaje =='5'){
+        swal(':(', 'mp fue posible eliminar el registro correctamente', 'error');
+      }
+    </script>
+        
   </body>
 </html>
-
-<script> 
-  lef mensaje = '<?php echo $mensaje; ?>';
-  if (mensaje == '0'){
-    swal(':D','registro insertado correctamente','success');
-  }else if (mensaje=='1'{
