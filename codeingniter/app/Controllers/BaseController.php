22d76fdba7dd61a8e8fe2e8ca74a6783bb294bd0
MarioGarcia7w7 <83962755+MarioGarcia7w7@users.noreply.github.com>
2021-06-17 13:20:27 -0500

    Update BaseController.php

--- codeingniter/app/Controllers/BaseController.php
+++ codeingniter/app/Controllers/BaseController.php
@@ -1,69 +1,76 @@
 <?php
+
 namespace App\Controllers;
 use App\Models\DatosModel;
-
-class General extends BaseController{
-	
-	public function index(){
-		$gModel = new DatosModel();
+class General extends BaseController
+{
+	public function index()
+	{
+		$model = new DatosModel();
 		$mensaje = session('mensaje');
-		$datos = $gModel ->listarTodo();
-		$mensaje = session('mensaje');
-		$datos =[$gModel->listarTodo();
-			 $data = ["datos" => $datos,
-                                  "mensaje"=>mensaje
-				 ];
-			 return view('listado',$data);
-			 }
-			 public funtion obtenerDatos($id){
-				 $gModel new DatosModel();
-			         $data = ["id" => $id];
-				 $respuesta = $gModel->obtenerInformacion($data);
-				 
-				 $datos =["datos"=>$respuesta];
-				 return view('actualizar',$datos);
+		$datos = $model->listarTodo();
+		$data = [
+                     "datos" => $datos,
+			"mensaje" => $mensaje
+		];
+		return view('listado',$data);
+	}
+}
+        public function obtenerDatos($id) {
+		$model = new DatosModel();
+		$data = ["id" => $id];
+		$respuesta = $model ->obtenerInformacion($data);
+		
+		$datos = ["datos" => $respuesta];
+		return view('actualizar', $datos);
+		
+	}
+        public function insertar(){
+		$model = new DatosModel();
+		$data = [
+			"nombre" => $_POST ['nombre'],
+			"a_paterno" => $_POST ['apaterno'],
+			"a_materno" => $_POST ['materno'],
+		];
+		$respuesta = $model ->insertar($data);
+		
+		if($respuesta > 0){
+			return redirect()->to(base_url('/index.php'))->with('mensaje','0');
+		}else{
+			return redirect()->to(base_url('/index.php'))->with('mensaje','1');
+		}
+	}
+       
 
-			 }
-			 public funtion insertar (){
-				 $gModel=new DatosModel();
-				 $data=[
-					 "nombre" =>$_POST['nombre'],
-					 "a_paterno"=> $_POST['apaterno'],
-					 "a_materno"=> $_POST['amaterno'],
-				 ];
-				 $respuesta = $gModel->insertar($data);
-				 if ($respuesta > 0){
-					 return redirect()-> to (base_url('/index.php'))->with('mensaje','0');
-				 }else{
-					 return redirect()-> to (base_url('/index.php'))->with('mensaje','1');
-				 }
-			 }
-			 
-			 public funtion actualizar();
-			 $gModel = new GeneralModel();
-			 $data = [
-				 "nombre" =>$_POST['nombre'],
-				 "a_paterno"=>$POST['apaterno'];
-				 "a_materno"=>$POST['amaterno'];
-				 ];
-			 $id = ["id" => $_POST['id']];
-			 $respuesta = $gModel->actualizar($data,$id);
-			 if ($respuesta){
-				 return redirect ()->to(base_url('/index.php'))->with('mensaje','2');
-			 }else{
-				  return redirect ()->to(base_url('/index.php'))->with('mensaje','3');
-			 }
-			 }
-			 public funtion eliminar($idPersona){
-				 $gModel = new DatosModel();
-				 $id = ["id" =>$idPersona];
-				 $respuesta = $gModel->eliminar($id);
-				 
-				 if($respuesta){
-					 return redirect ()->to(base_url('index,php'))->with('mensaje','4');
-				 }else{
-					 return redirect ()->to(base_url('index,php'))->with('mensaje','5'); 
-				 }
-				 
-			 }
-			 }
+
+
+        public function actualizar(){
+		$model = new DatosModel();
+		$data = [
+			"nombre" => $_POST ['nombre'],
+			"a_paterno" => $_POST ['paterno'],
+			"a_materno" => $_POST ['materno'],
+			
+			
+		];
+		$id = ["id" => $_POST['id']];
+		$respuesta = $model->actualizar($data,$id);
+		
+		if($respuesta){
+			return redirect()->to(base_url('/index.php'))->with('mensaje','2');
+		}else{ 
+			return redirect()->to(base_url('/index.php'))->with('mensaje','3');
+		}
+	}
+        public function eliminar($idPersona){
+		$model = new DatosModel();
+		$id = [ "id" => $idPersona];
+		$respuesta = $model->eliminar($id);
+		
+		if($respuesta){
+			return redirect()->to(base_url('/index.php'))->with('mensaje','4');
+		}else{
+			return redirect()->to(base_url('/index.php'))->with('mensaje','5');
+		}
+	}
+}
